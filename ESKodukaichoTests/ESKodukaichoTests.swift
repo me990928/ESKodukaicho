02ac9bd5a12f4d25fcc75c27192dc5b27b86ecf8
me990928 <me990928@gmail.com>
2024-09-29 13:35:16 +0900

    Initial Commit

---
+++ ESKodukaichoTests/ESKodukaichoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ESKodukaichoTests.swift
+//  ESKodukaichoTests
+//
+//  Created by 広瀬友哉 on 2024/09/29.
+//
+
+import Testing
+@testable import ESKodukaicho
+
+struct ESKodukaichoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
