c04e6f86e2541b715bd28abf73956a9ea3fb99ef
me990928 <me990928@gmail.com>
2024-10-02 13:56:53 +0900

    配色の修正

---
+++ ESKodukaicho/Sheet/Input/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  ESKodukaicho
+//
+//  Created by 広瀬友哉 on 2024/10/02.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
