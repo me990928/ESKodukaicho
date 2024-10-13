80252010958539cfae03c2bb866be22a3dd7b8ff
me990928 <me990928@gmail.com>
2024-10-13 16:44:16 +0900

    #28 定数の洗い出し

---
+++ ESKodukaicho/Enums/PaymentsGenre.swift
@@ -0,0 +1,12 @@
+//
+//  PaymentsGenre.swift
+//  ESKodukaicho
+//
+//  Created by 広瀬友哉 on 2024/10/13.
+//
+
+enum PaymentsGenre: String, Codable {
+    case pay = "支出"
+    case salary = "収入"
+    case save = "貯金"
+}
